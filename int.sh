dbc64acadae3c4709e53a6a01fe1b7b9fba68a98
Anushya <anushya.vp19@gmail.com>
2024-10-28 17:06:47 +0530

    variables

--- int.sh
+++ int.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 PERSON1=$1
 PERSON2=$2
 
--- variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PERSON1=$1
-PERSON2=$2
-
-
-echo " $PERSON1: hi $PERSON2 how are you?"
-echo " $PERSON2 : hi $PERSON1 im fine "
-echo " $PERSON1 : $PERSON2 what are you doing"
-echo " $PERSON2 : $PERSON1 im watching tv"
