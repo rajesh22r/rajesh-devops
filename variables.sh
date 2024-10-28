ae48f064511c7463f68131a0ecb36ec8ed1f36c8
Anushya <anushya.vp19@gmail.com>
2024-10-28 16:45:49 +0530

    printed convo btw ramesh and suresh

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+
+echo " $person1: hi $person2 how are you?"
+echo " $person2 : hi $person1 im fine "
+echo " $person1 : $person2 what are you doing"
+echo " $person2 : $person1 im watching tv"
