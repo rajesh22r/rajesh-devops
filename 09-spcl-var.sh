b910072180831fd9b1f25f759ea2c77363180bce
Anushya <anushya.vp19@gmail.com>
2024-10-29 13:03:49 +0530

    message

--- 09-spcl-var.sh
+++ 09-spcl-var.sh
@@ -4,7 +4,7 @@ echo "all variables passed to the script : $@"
 echo "number of variables passed : $#"
 echo "script name : $0"
 echo "current working directory : $PWD"
-echo "home directory of user : $home"
+echo "home directory of user : $HOME"
 echo "pid of script executing now: $$"
 sleep 100 &
 echo "pid of last background command: $!"
