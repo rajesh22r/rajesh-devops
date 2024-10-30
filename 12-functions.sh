838f98a289bf1780c6bc962a2fb176369c25aa7a
Anushya <anushya.vp19@gmail.com>
2024-10-30 12:26:44 +0530

    message

--- 12-functions.sh
+++ 12-functions.sh
@@ -19,13 +19,7 @@ if [ $? -ne 0 ]
 then 
    echo "git is not installed, going to install it"
    dng install git -y
-   if [ $? -ne 0 ]
-   then 
-      echo "git installation is not success... check it"
-      exit 1
-   else
-      echo "git installation is success"
-   fi
+   validate $? "installing git"
 else 
    echo "git is already installed"
 fi
