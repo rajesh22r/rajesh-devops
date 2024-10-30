6d9aee32a36b16331c292485a53beb27721d714b
Anushya <anushya.vp19@gmail.com>
2024-10-30 12:13:13 +0530

    message

---
+++ 12-functions.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then 
+  echo "please run through root privileges"
+  exit 1
+fi
+validate (){
+    if [ $1 -ne 0 ]
+    then 
+       echo "$2 is failed.. check it "
+    else 
+       echo " $2 is success"
+    fi
+}
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+   echo "git is not installed, going to install it"
+   dng install git -y
+   if [ $? -ne 0 ]
+   then 
+      echo "git installation is not success... check it"
+      exit 1
+   else
+      echo "git installation is success"
+   fi
+else 
+   echo "git is already installed"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+   echo "mysql is not installed.. going to install it"
+   dnf install mysql -y
+   validate $? "installing mysql"
+else 
+   echo "mysql is already installed"
+fi
