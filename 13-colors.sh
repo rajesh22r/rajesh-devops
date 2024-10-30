bc531cbe60e16241b59252b90d3a94cba4ca7d58
Anushya <anushya.vp19@gmail.com>
2024-10-30 12:36:55 +0530

    message

---
+++ 13-colors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then 
+  echo "please run through root privileges"
+  exit 1
+fi
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+validate (){
+    if [ $1 -ne 0 ]
+    then 
+       echo "$2 is $R failed.. $N check it "
+    else 
+       echo " $2 is $G success "
+    fi
+}
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+   echo "git is not installed, going to install it"
+   dng install git -y
+   validate $? "installing git"
+else 
+   echo "git is already installed"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+   echo "mysql is not installed.. going to install it"
+   dnf install mysql -y
+   validate $? "installing mysql"
+else 
+   echo "mysql is already installed"
+fi
