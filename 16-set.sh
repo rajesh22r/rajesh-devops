60711711a3159519ebbfe48df84d36266a154820
Anushya <anushya.vp19@gmail.com>
2024-11-11 12:26:34 +0530

    message

--- 16-set.sh
+++ 16-set.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -ex
+failure(){
+    echo "failed at line $1 : $2"
 
+}
 
-trap '' Err
+trap ' "$LINENO" "$BASH_COMMAND" ' Err
 echo "hello world"
 echoo "hello world failure"
 echo "hello aftr failure"
